3951ea3eb4293177755f2da42fb8a21f43e34437
Surajit Chongder <surajit.chongder@gmail.com>
2016-02-18 23:22:18 +0530

    basic multiplication for two same size matrix added

--- Matrix.java
+++ Matrix.java
@@ -1,24 +1,23 @@
-import java.util.Arrays;
 class Matrix{
-  private int row;
-  private int column;
+  private int rowSize;
+  private int columnSize;
   private int[][] matrix;
 
-  public Matrix(int insertedRow,int insertedColumn,int [] userData){
-    row = insertedRow;
-    column = insertedColumn;
-    matrix= new int[row][column];
-    matrix = this.createMatrix(userData);
+  public Matrix(int rowSize,int columnSize,int [] data){
+    this.rowSize = rowSize;
+    this.columnSize = columnSize;
+    this.matrix = this.initialize(data);
   }
 
-  private int [][] createMatrix (int [] matrixData){
+  private int [][] initialize (int [] matrixData){
     int index;
     int initialRow = 0;
     int initialColumn = 0;
+    matrix = new int [rowSize][columnSize];
     for (index = 0 ; index < matrixData.length ; index++ ){
       matrix[initialRow][initialColumn]=matrixData[index];
       initialColumn++;
-      if (initialColumn == column) {
+      if (initialColumn == columnSize) {
         initialRow++;
         initialColumn = 0;
       }
@@ -28,21 +27,36 @@ class Matrix{
 
   public Matrix add(Matrix second){
     int [][]addent = second.matrix;
-    int [] result = new int[row * column];
+    int [] result = new int[rowSize * columnSize];
     int index;
     int initialRow = 0;
     int initialColumn = 0;
-    for (index = 0 ; index < row*column ; index++ ){
+    for (index = 0 ; index < result.length ; index++ ){
       result[index] = matrix[initialRow][initialColumn] + addent[initialRow][initialColumn];
       initialColumn++;
-      if (initialColumn == column) {
+      if (initialColumn == columnSize) {
         initialRow++;
         initialColumn = 0;
       }
     }
-    return new Matrix(row,column,result);
+    return new Matrix(rowSize,columnSize,result);
   }
 
+  public Matrix multiply(Matrix second){
+    int sum=0,index,initialRow,initialColumn,initialIndex=0;
+    int [] multiplyResult = new int[this.rowSize*second.columnSize];
+    for (index = 0 ; index < this.rowSize ; index++ ){
+          for ( initialRow = 0 ; initialRow < second.columnSize ; initialRow++ ){
+             for ( initialColumn = 0 ; initialColumn < this.rowSize ; initialColumn++ ){
+                sum = sum + this.matrix[index][initialColumn]*second.matrix[initialColumn][initialRow];
+             }
+             multiplyResult[initialIndex] = sum;
+             initialIndex++;
+             sum = 0;
+          }
+    }
+    return new Matrix(this.rowSize,second.columnSize,multiplyResult);
+  }
 
   public int getElement(int rowPosition,int columnPosition){
     return matrix[rowPosition][columnPosition];
--- MatrixTest.java
+++ MatrixTest.java
@@ -17,9 +17,22 @@ public class MatrixTest {
     Matrix matrix1 = new Matrix (2,2,data1);
     Matrix matrix2 = new Matrix (2,2,data2);
     Matrix result = matrix1.add(matrix2);
-    int [] expect = {5,7,9,7};
-    assertEquals(expect[3],result.getElement(1,1));
-    assertEquals(expect[2],result.getElement(1,0));
+
+    assertEquals(7,result.getElement(1,1));
+    assertEquals(9,result.getElement(1,0));
+  }
+  @Test
+  public void matrixMultiplicationTestForTwoEqualSizeMatrix() {
+    int []data1 = {2,3,4,3};
+    int []data2 = {3,4,5,4};
+    Matrix matrix1 = new Matrix (2,2,data1);
+    Matrix matrix2 = new Matrix (2,2,data2);
+    Matrix result = matrix1.multiply(matrix2);
+
+    assertEquals(21,result.getElement(0,0));
+    assertEquals(20,result.getElement(0,1));
+    assertEquals(27,result.getElement(1,0));
+    assertEquals(28,result.getElement(1,1));
   }
 
 }
